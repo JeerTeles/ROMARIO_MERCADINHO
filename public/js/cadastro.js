60da59d7a1e3cb69e5af0609126a7b8f8fea23e5
JeerTeles <jadersoncontatos@gmail.com>
2025-06-17 00:59:16 -0300

    feature/do not display the id field in the registered customer table

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -34,10 +34,10 @@ document.addEventListener('DOMContentLoaded', () => {
             totalPages = result.totalPages;
             renderClientsList(result.data);
             updatePaginationControls();
-
+            
         } catch (error) {
             console.error('Erro ao buscar clientes:', error);
-            clientesListBody.innerHTML = `<tr><td colspan="6" id="noClientFound" style="color: red;">Erro ao carregar clientes: ${error.message}</td></tr>`;
+            clientesListBody.innerHTML = `<tr><td colspan="6" id="noClientFound" style="color: red;">Erro ao carregar clientes, sem servidor! </td></tr>`;
             pageInfoSpan.textContent = 'Erro';
             prevPageBtn.disabled = true;
             nextPageBtn.disabled = true;
@@ -56,9 +56,9 @@ document.addEventListener('DOMContentLoaded', () => {
         clients.forEach((client) => {
             const row = document.createElement('tr');
 
-            const idCell = document.createElement('td');
+            /*const idCell = document.createElement('td');
             idCell.textContent = client.id;
-            row.appendChild(idCell);
+            row.appendChild(idCell);*/
 
             const nomeCell = document.createElement('td');
             nomeCell.textContent = client.nomeCliente;
